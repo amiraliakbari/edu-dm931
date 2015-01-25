5591c52b5b554ed5ae4937a5690bf2c16dd2c020
Baharloo <baharloo.omid@gmail.com>
2015-01-25 15:32:35 +0330

    question 1,2

---
+++ convert.R
@@ -0,0 +1,31 @@
+library('RMySQL');
+library('sqldf');
+con = dbConnect(dbDriver("MySQL"), user = "root", password = "", dbname = "movies2");
+allData=dbGetQuery(con,'SELECT * FROM ratings');
+avgs=dbGetQuery(con,'SELECT MovieID,count(Rating),avg(Rating) FROM ratings group by MovieID');
+movies=avgs$MovieID;
+users=unique(allData$UserID);
+hor=array(NA,dim=c(length(users),length(movies)));
+rownames(hor)=users;
+colnames(hor)=movies;
+rMovies=allData$MovieID;
+rUsers=allData$UserID;
+rRatings=allData$Rating;
+len=length(rMovies);
+for(i in 1:len){
+  hor[toString(rUsers[i]),toString(rMovies[i])]=rRatings[i];
+}
+uLen=length(users);
+newTabel=array(0,dim=c(uLen,uLen));
+for(i in 1:uLen-1){
+  for(j in (i+1):uLen){
+    delta=hor[i,]-hor[j,];
+    delta[is.na(delta)]=0
+    newTabel[j,i]=sqrt(sum(delta^2));
+  }
+  print(i);
+}
+
+window=newTabel[1:60,1:60];
+h=hclust(as.dist(window));
+clus=cutree(h,k=6);
--- dm.R
+++ dm.R
@@ -1,14 +1,53 @@
-rating <- read.csv("C:/Users/Omid/Documents/DM PROJECT/rating.csv");
-I=rating[((rating$MovieID==2812638)),c('UserID','MovieID','Rating')];
-J=rating[,c('MovieID')];
-movies=unique(J);
-mean_table=array(0,dim=c(2,length(movies)));
-ite=1;
-for(m in movies){
-  rs=rating[((rating$MovieID==m)),c('Rating')];
-  r=mean(rs);
-  mean_table[1,ite]=m;
-  mean_table[2,ite]=r;
-  ite=ite+1;
+library('RMySQL');
+library('sqldf');
+con = dbConnect(dbDriver("MySQL"), user = "root", password = "", dbname = "movies2");
+allData=dbGetQuery(con,'SELECT * FROM ratings');
+avgs=dbGetQuery(con,'SELECT MovieID,count(Rating),avg(Rating) FROM ratings group by MovieID');
+movies=avgs$MovieID;
+users=unique(allData$UserID);
+#mean=avgs[avgs$MovieID==19876,c('avg(Rating)')];
+#sData=allData[(allData$MovieID==2812638&allData$UserID==24),c('Rating')];
+#newTable=array(0,dim=c(length(users),length(users));
+#uIte=1;
+#for(u in users){
+#  newTable[uIte,1]=u;
+#  uIte=uIte+1;
+#}
+
+#mIte=1;
+#for(m in movies){
+ # uIte=1;
+  #mean=avgs[avgs$MovieID==m,c('avg(Rating)')];
+  #for(u in users){
+  #  sData=allData[(allData$MovieID==m&allData$UserID==u),c('Rating')];
+   # if(length(sData)>0){
+  #  newTable[uIte,mIte+1]=sData;
+  #  print("found");
+  #  }else{  
+  #    newTable[uIte,mIte+1]=mean;
+  #  }
+   # uIte=uIte+1;
+  #}
+  #mIte=mIte+1;
+  #print(mIte);
+#}
+
+#uLen=length(users);
+uLen=20;
+newTabel=array(0,dim=c(40,40));
+for(i in 1:19){
+  origin=users[i];
+  mOrigin=allData[allData$UserID==origin,c('MovieID','Rating')];
+  for(j in i+1:20){
+    other=users[j];
+    mOther=allData[allData$UserID==other,c('MovieID','Rating')];
+    inte=intersect(mOrigin$MovieID,mOther$MovieID);
+   # I1=which(mOrigin$MovieId==inte);
+    #I2=which(mOther$MovieId==inte);
+    r1=mOrigin[mOrigin$MovieID==inte,c('Rating')];
+  r2=mOther[mOther$MovieID==inte,c('Rating')];
+  delta=r1-r2;
+  newTabel[i,j]=sqrt(sum(delta^2));
+  }
 }
-ply(rating, c("MovieID"), summarize, rating$Rating == mea
+
